faa622f12b400a19f930f463f635c6d2da6bef35
Peter Jung <admin@ptr1337.dev>
2024-11-17 15:22:00 +0100

    cachy
    Signed-off-by: Peter Jung <admin@ptr1337.dev>

--- block/bfq-iosched.c
+++ block/bfq-iosched.c
@@ -7656,6 +7656,7 @@ MODULE_ALIAS("bfq-iosched");
 static int __init bfq_init(void)
 {
 	int ret;
+	char msg[60] = "BFQ I/O-scheduler: BFQ-CachyOS v6.11";
 
 #ifdef CONFIG_BFQ_GROUP_IOSCHED
 	ret = blkcg_policy_register(&blkcg_policy_bfq);
@@ -7686,6 +7687,11 @@ static int __init bfq_init(void)
 	ret = elv_register(&iosched_bfq_mq);
 	if (ret)
 		goto slab_kill;
+
+#ifdef CONFIG_BFQ_GROUP_IOSCHED
+	strcat(msg, " (with cgroups support)");
+#endif
+	pr_info("%s", msg);
 
 	return 0;
 
